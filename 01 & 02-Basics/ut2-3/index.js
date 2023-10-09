491c6ff892e6d95b8cdc58e1ceaadab35d86acf1
josemird <josemidaw22@gmail.com>
2023-10-09 09:20:55 +0200

    09-10-23

---
+++ 01 & 02-Basics/ut2-3/index.js
@@ -0,0 +1,13 @@
+// OBJETO LITERAL 
+let gato={
+    nombre:"Paquito",
+    edad:"23",
+    raza:"Persa",
+    sexo:"Macho",
+    color:"blanco",
+    peso_kg:"5",
+    largo_cm:"50"
+}
+
+console.log(Object.keys(gato));
+console.log(Object.values(gato));
---
+++ 03-DOM/ut3-4 DOM Attributes/main.js
@@ -0,0 +1,23 @@
+const body = document.getElementsByTagName("Body");
+
+const div = document.createElement('div');
+const a1 = document.createElement('a');
+const a2 = document.createElement('a');
+const a3 = document.createElement('a');
+const a4 = document.createElement('a');
+const a5 = document.createElement('a');
+const a6 = document.createElement('a');
+const a7 = document.createElement('a');
+const a8 = document.createElement('a');
+const a9 = document.createElement('a');
+
+document.body.append(div);
+document.div.append(a1);
+document.div.append(a2);
+document.div.append(a3);
+document.div.append(a4);
+document.div.append(a5);
+document.div.append(a6);
+document.div.append(a7);
+document.div.append(a8);
+document.div.append(a9);
