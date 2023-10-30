bc2a966b8c868b270de5615bdc84f59dce89001c
josemird <josemidaw22@gmail.com>
2023-10-30 12:41:14 +0100

    push

---
+++ 4-OOP/2-OOP(a)/books.js
@@ -0,0 +1,22 @@
+export const books = [
+    {
+        title: "La vida del lazarillo de Tormes",
+        author: "Anonymous",
+        published: "1554",
+    },
+    {
+        title: "The NeverEnding Story",
+        author: "Michael Ende",
+        published: "1954",
+    },
+    {
+        title: "The Lord of the Rings",
+        author: "J. R. R. Tolkien",
+        published: "1954",
+    },
+    {
+        title: "Beowulf",
+        author: "Anonymous",
+        published: "900",
+    },
+];
---
+++ 4-OOP/2-OOP(a)/main.js
@@ -0,0 +1,11 @@
+import * as book from "./books.js";
+
+class books{
+    #title;
+    #author;
+    constructor(title, author){
+        this.#title = title,
+        this.#author = author,
+        this.published = published;
+    }
+}
