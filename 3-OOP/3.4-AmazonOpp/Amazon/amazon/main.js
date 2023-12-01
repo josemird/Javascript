35a448089e8acece1e33b5c74136a2b96354f04b
josemird <josemidaw22@gmail.com>
2023-12-01 21:22:42 +0100

    commit amazon

--- 3-OOP/3.4-AmazonOpp/Amazon/amazon/main.js
+++ 3-OOP/3.4-AmazonOpp/Amazon/amazon/main.js
@@ -1,28 +1,36 @@
-import {Price} from './Classes/Price.js';
-import {Product} from './Classes/Product.js';
-import {Review} from './Classes/Review.js';
+// main.js
+import { Price } from './Classes/Price.js';
+import { Product } from './Classes/Product.js';
+import { Review } from './Classes/Review.js';
 
-let price = new Price("USD", 339);
-let product = new Product(
-  "New Apple iPhone SE (64GB, (Product) RED) [Locked] + Cricket Wireless Plan",
-  "https://www.amazon.com/dp/B087623TMW",
-  "https://m.meda-amazoncom/images/l/810DvHOZ9nL—AC_UL320_.jpg"
-)
-let review = new Review("4.6", 73);
-//Recupero el Div del HTML
-const container = document.getElementById('container');
+document.addEventListener('DOMContentLoaded', function () {
+  let price = new Price("USD", 339);
+  let product = new Product(
+    "New Apple iPhone SE (64GB, (Product) RED) [Locked] + Cricket Wireless Plan",
+    "https://www.amazon.com/dp/B087623TMW",
+    "https://m.meda-amazoncom/images/l/810DvHOZ9nL—AC_UL320_.jpg"
+  );
+  let review = new Review("4.6", 73);
 
-//Muestro los Elementos en Parrafos 
-const priceElement = document.createElement('p');
-priceElement.textContent = `Price: ${price.currency}`;
+  // Recupero el Div del HTML
+  const container = document.getElementById('container');
 
-const productElement = document.createElement('p');
-productElement.textContent = `Product: ${product.toString()}`;
+  if (container) {
+    // Muestro los Elementos en Párrafos
+    const priceElement = document.createElement('p');
+    priceElement.textContent = `Price: ${price.current_price} ${price.currency}`;
 
-const reviewElement = document.createElement('p');
-reviewElement.textContent = `Review: ${review.toString()}`;
+    const productElement = document.createElement('p');
+    productElement.textContent = `Product: ${product.title}`;
 
-// Agregar los elementos al Div
-container.appendChild(priceElement);
-container.appendChild(productElement);
-container.appendChild(reviewElement);
+    const reviewElement = document.createElement('p');
+    reviewElement.textContent = `Review: ${review.rating} (${review.total_reviews} reviews)`;
+
+    // Agregar los elementos al Div
+    container.appendChild(priceElement);
+    container.appendChild(productElement);
+    container.appendChild(reviewElement);
+  } else {
+    console.error('El elemento con ID "container" no se encontró en el DOM.');
+  }
+});
