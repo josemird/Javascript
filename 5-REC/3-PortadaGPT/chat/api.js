055bb19b976cbebef36639b1a841169a6fd8c054
josemird <josemidaw22@gmail.com>
2024-04-23 11:15:53 +0200

    push chat

---
+++ 5-REC/3-PortadaGPT/chat/api.js
@@ -0,0 +1,21 @@
+export const urlApiChat = 'https://chatgpt-best-price.p.rapidapi.com/v1/chat/completions';
+
+export let getOptions = (textPrompt) => {
+    return {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': 'e76c1e965dmsh65c7514abaf1d2bp14f042jsned57669d1b0e',
+            'X-RapidAPI-Host': 'chatgpt-best-price.p.rapidapi.com'
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'user',
+                    content: textPrompt,
+                }
+            ]
+        }),
+    }
+};
--- 5-REC/3-PortadaGPT/chat/main.js
+++ 5-REC/3-PortadaGPT/chat/main.js
@@ -1,24 +1,56 @@
+import { urlApiChat, getOptions} from './api';
 import './style.css'
 
 const buttonGenerator =  document.querySelector('button')
+const chatgptContainer = document.querySelector('.chatgpt-container');
+const textQuestion = document.querySelector('textarea')
   buttonGenerator.disabled = true; //propiedad disables activada por defecto
   buttonGenerator.className = 'button-generator'
 
-const chatgptContainer = document.querySelector('.chatgpt-container');
 
+//RESPONSE ELEMENT CREATION
 const createResponseElement = () => {
   const responseContainer = document.createElement('div');
     responseContainer.className = 'response-container';
 
   chatgptContainer.appendChild(responseContainer);
 
-  const textResponse = document.createElement('textarea');
+  const textResponse = document.createElement('textarea'); //texto respuesta
     textResponse.setAttribute('placeholder','Response');
     textResponse.setAttribute('rows', 5);
 
   responseContainer.appendChild(textResponse);
+
+  //todo: añadir los botones de bootstrap aqui
+  //Iconos Bootstrap
+  let icon = document.createElement('i')
+  let iconPlus = document.createElement('i');
+  let iconLess = document.createElement('i')
+
+  textResponse.appendChild(icon);
+  textResponse.appendChild(iconPlus);
+  textResponse.appendChild(iconLess);
 };
 createResponseElement();
+
+
+//BUTTON ACTIVE EVENT
+textQuestion.addEventListener('input', () => {
+  (textQuestion.value === "" || textQuestion.value.trim().length === 0)
+    ? (buttonGenerator.disabled = true)
+    : (buttonGenerator.disabled = false);
+})
+
+//DATOS API PROMPT
+buttonGenerator.addEventListener('click', async() => {
+  const response = await fetch(urlApiChat, getOptions(textQuestion.value));
+  const data = await response.json();
+  const dataResult = data.choices[0].message.content;
+  console.log(dataResult);
+})
+
+
+
 
 
 
