5beed9e8f27748805b1b1bc7cd7c278d4029241f
josemird <josemidaw22@gmail.com>
2024-04-28 13:44:45 +0200

    push chat

--- 5-REC/3-PortadaGPT/chat/main.js
+++ 5-REC/3-PortadaGPT/chat/main.js
@@ -1,14 +1,15 @@
 import { urlApiChat, getOptions} from './api';
 import './style.css'
 
-const buttonGenerator =  document.querySelector('button')
 const chatgptContainer = document.querySelector('.chatgpt-container');
 const textQuestion = document.querySelector('textarea')
+  textQuestion.className = 'text-question';
+const buttonGenerator =  document.querySelector('button')
   buttonGenerator.disabled = true; //disable property: active by default
   buttonGenerator.className = 'button-generator'
 
 
-//RESPONSE ELEMENT CREATION
+//*RESPONSE ELEMENT CREATION
 const createResponseElement = () => {
   const responseContainer = document.createElement('div');
     responseContainer.className = 'response-container';
@@ -16,12 +17,13 @@ const createResponseElement = () => {
   chatgptContainer.appendChild(responseContainer);
 
   const textResponse = document.createElement('textarea'); //response text
+    textResponse.className = 'text-response';
     textResponse.setAttribute('placeholder','Response');
     textResponse.setAttribute('rows', 5);
 
   responseContainer.appendChild(textResponse);
 
-  //BOOTSTRAP'S ICONS
+  //*BOOTSTRAP'S ICONS
   const iconsContainer = document.createElement('div');
   iconsContainer.className = 'icons-container';
   responseContainer.appendChild(iconsContainer);
@@ -40,20 +42,30 @@ const createResponseElement = () => {
 createResponseElement();
 
 
-//BUTTON ACTIVE EVENT
+//*BUTTON ACTIVE EVENT
 textQuestion.addEventListener('input', () => {
   (textQuestion.value === "" || textQuestion.value.trim().length === 0)
     ? (buttonGenerator.disabled = true)
     : (buttonGenerator.disabled = false);
 })
 
-//API PROMPT DATA
+//*API PROMPT DATA EVENT (BUTTON)
 buttonGenerator.addEventListener('click', async() => {
   const response = await fetch(urlApiChat, getOptions(textQuestion.value));
   const data = await response.json();
   const dataResult = data.choices[0].message.content;
-  console.log(dataResult);
-})
+  //console.log(dataResult);
+  const textResponse = document.querySelector('.text-response');
+  textResponse.value = ''
+  //textResponse.value = dataResult; // without animation typing effect
+  let letters = 0; 
+  const typing = setInterval(() => {
+    textResponse.value += dataResult[letters];
+    letters++;
+    if (letters >= dataResult.length) {clearInterval(typing)}
+  }, 33); //* with animation typing effect
+  
+});
 
 
 
