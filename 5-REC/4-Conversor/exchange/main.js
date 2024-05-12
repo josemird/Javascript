9ecea10a03e501816556aace1495fa43f2f16677
José Miguel <josemidaw22@gmail.com>
2024-05-12 14:41:32 +0200

    select done

--- 5-REC/4-Conversor/exchange/main.js
+++ 5-REC/4-Conversor/exchange/main.js
@@ -11,17 +11,32 @@ const divContainer = document.querySelector('.container')
 //create elements & attributes
 const divSelectContainer = document.createElement('div');
     divSelectContainer.className = 'select_container';
-const select = document.createElement('select');
+const selectTagUp = document.createElement('select');
+const selectTagDown = document.createElement('select');
 //appenchilds
 divContainer.appendChild(divSelectContainer);
-divSelectContainer.appendChild(select);
-
-const currenciesSupportedResponse = Object.values(currenciesSupported.data)
-const currenciesSupportedArray = currenciesSupportedResponse.forEach((currency)=>{
-    console.log(currency.name)
-});
-console.log(currenciesSupportedArray);
+divSelectContainer.appendChild(selectTagUp);
+divSelectContainer.appendChild(selectTagDown);
 
 //todo: crear los option y mostrarlos como select, hay que hacer 2 select, probablemente
-//todo:+ haran falta 2 funciones
+//todo:+ haran falta 2 funcione
+//* FUNCIONES CREAR CAMPOS EN LOS SELECT
+const currenciesSupportedResponse = Object.values(currenciesSupported.data) //obtenemos un array de los valores del objeto 'currenciesSupported' -> 'data'
+currenciesSupportedResponse.forEach((currency)=>{  //iteramos sobre el array de valores para obtener el nombre de cada moneda y crear un option con ese nombre, y lo añadimos al select
+    //console.log(currency.name)
+    const option = document.createElement('option');
+    selectTagUp.appendChild(option)
+    option.value = currency.name;
+    option.textContent = currency.name;
+});
+
+currenciesSupportedResponse.forEach((currency)=>{  //repetimos el proceso para el segundo select
+    //console.log(currency.name)
+    const option = document.createElement('option');
+    selectTagDown.appendChild(option)
+    option.value = currency.name;
+    option.textContent = currency.name;
+});
+s
+
 
