285cbfc848b7f79ca770f4b7867afefadb8eff6a
josemird <josemidaw22@gmail.com>
2024-05-21 11:02:41 +0200

    push

--- 5-REC/4-Conversor/exchange/domHelper.js
+++ 5-REC/4-Conversor/exchange/domHelper.js
@@ -1,21 +1,14 @@
-import { currenciesSupportedResponse, selectTagUp, selectTagDown } from './main';
-
 //* FUNCIONES PARA CREAR LOS CAMPOS EN LOS SELECT
-export const selectUpCreation = () => {                     // Función que crea los campos en el select de monedas
-    currenciesSupportedResponse.forEach((currency) => {     // Recorremos el array de monedas soportadas
-            //console.log(currency.name)
-        const option = document.createElement('option'); 
-        selectTagUp.appendChild(option);
-        option.value = currency.name;           // Asignamos el valor de la moneda
-        option.textContent = currency.name;     // Asignamos el texto de la moneda
-    });
-}
-export const selectDownCreation = () => { 
+ 
+
+export const selectCreation = (currenciesSupportedResponse, select) => { 
     currenciesSupportedResponse.forEach((currency) => { 
             //console.log(currency.name)
         const option = document.createElement('option');
-        selectTagDown.appendChild(option);
+        select.appendChild(option);
         option.value = currency.name;
         option.textContent = currency.name;
     });
 };
+
+
